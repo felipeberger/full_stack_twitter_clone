5994f1b9dc118a1ea815fdc9b5d8d5e0566add91
felipeberger <feconchaber@gmail.com>
2021-06-24 10:49:03 +1100

    Updates to styling in scss files. Updates to home.jsx include styling of existing components and finalizing the look and structure of the main feed area. No functionality has been added yet.

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -7,6 +7,7 @@ import MenuSelect from './homeMenu.jsx'
 import './home.scss';
 import './general.scss';
 
+// to be updated with code once backend is connected
 let username = "felipe"
 let tweetsCount = 1
 let followingCount = 0
@@ -23,19 +24,19 @@ const Home = (props) => {
         <div className="row">
           <div className="col-3" id="sidebar">
             <div className="bg-light rounded pl-2 pt-2 mb-3">
-              <h5 className="font-weight-bold">{username}</h5>
-              <p>@{username}</p>
-              <div className="row pb-3">
+              <a className="font-weight-bold text-dark" href=""><h5 className="">{username}</h5></a>
+              <a className="text-secondary" href="">@{username}</a>
+              <div className="row py-3">
                 <div className="col-4 pr-0">
-                  <p className="font-weight-light mb-0">TWEETS</p>
+                  <p className="category-heading font-weight-light mb-0">TWEETS</p>
                   <a href="">{tweetsCount}</a>
                 </div>
                 <div className="col-4 px-0">
-                  <p className="font-weight-light mb-0">FOLLOWING</p>
+                  <p className="category-heading font-weight-light mb-0">FOLLOWING</p>
                   <a href="">{followingCount}</a>                
                 </div>
                 <div className="col-4 pl-0">
-                  <p className="font-weight-light mb-0">FOLLOWERS</p>
+                  <p className="category-heading font-weight-light mb-0">FOLLOWERS</p>
                   <a href="">{followersCount}</a>
                 </div>
               </div>
@@ -51,8 +52,19 @@ const Home = (props) => {
               </div>
             </div>
           </div>
-          <div className="col-6" id="feed">
-            feed
+          <div className="col-6 feed rounded pt-2">
+            <div className="rounded">
+              <textarea className="form-control" id="tweet-input" cols="30" rows="2"></textarea>
+              <div className="text-right">                
+                <button className="rounded border-0 bg-transparent mr-3 font-weight-bold">Upload image</button>
+                <span className="letter-count font-weight-light">140</span>
+                <button className="rounded btn-primary px-2 ml-3 mr-2 my-2" id="post-tweet">Tweet</button>
+              </div>
+              <div id="tweets-feed">
+                  <p className="bg-light">feed goes here</p>
+              </div>
+              
+            </div>
           </div>
           <div className="col-3">
             {/* empty column to center feed and sidebar */}
