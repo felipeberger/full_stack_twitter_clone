74f551309a003effc3017a8d1d60c0cac207b4d9
felipeberger <feconchaber@gmail.com>
2021-06-23 14:01:48 +1100

    initial version of the home page dropdown menu, no functionality yet

---
+++ app/javascript/src/homeMenu.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MenuSelect = (props) => {
+    return (
+        <>
+        <div className="dropdown">
+
+            <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{props.username}</button>
+
+
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <button className="dropdown-item" type="button">{props.username}</button>
+                <hr />
+                <button className="dropdown-item" type="button">Lists</button>
+                <hr />
+                <button className="dropdown-item" type="button">Help</button>
+                <button className="dropdown-item" type="button">Keyboard Shortcuts</button>
+                <hr />
+                <button className="dropdown-item" type="button">Settings</button>
+                <button className="dropdown-item" type="button">Log Out</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default MenuSelect;
